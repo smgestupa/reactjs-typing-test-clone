c1ae95bc6806b7d0ae1346afd35413b0fa3a2c40
smgestupa <thereal.nwahs@gmail.com>
2024-02-07 17:54:46 +0800

    integrate react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom';
+import './index.css';
+
+// Routes
+import Index from './routes/index';
+import Leaderboard from './routes/leaderboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Index />
+  },
+  {
+    path: "/leaderboard",
+    element: <Leaderboard />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/routes/leaderboard.jsx
@@ -0,0 +1,7 @@
+export default function Leaderboard() {
+    return (
+        <>
+            <div>This is the leaderboard page!</div>
+        </>
+    )
+}
